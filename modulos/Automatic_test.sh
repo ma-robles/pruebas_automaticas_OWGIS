dcfa275e8c3191ef616adeb3998dd4895c7a8041
Pablocg0 <pablocg064@gmail.com>
2019-05-06 16:31:49 -0500

    Se agregaron las versiones de las bibliotecas que se usan

--- modulos/Automatic_test.sh
+++ modulos/Automatic_test.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+
+# 1 = OWGIS Global
+# 2 = OWGIS meteorologia
+# 3 = OWGIS Oleaje
+# 4 = los 3 anteriores
 source ~/anaconda3/envs/tensorflow/bin/activate selenuim
 python ../Lib/Automatic_test.py 4
